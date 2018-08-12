3a59309f50d19428cfcd672fd081b8ea24e3d3df
Olga Krayevska <tourist_79495@ukr.net>
2018-08-12 19:10:10 +0300

    adding solutions

--- 6kyu-Multiples-of-3-or-5.py
+++ 6kyu-Multiples-of-3-or-5.py
@@ -9,7 +9,6 @@ Finish the solution so that it returns
 the sum of all the multiples of 3 or 5 below the number passed in.
 Note: If the number is a multiple of both 3 and 5, 
 only count it once.
-
 """
 
 def solution(number):
