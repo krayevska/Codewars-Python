2d0a3511ebbf5e1406b5254f19311b29e4ec47ef
Olga Krayevska <tourist_79495@ukr.net>
2018-09-13 15:31:43 +0300

    adding solutions #100daysofcode

---
+++ 7kyu-Descending-Order.py
@@ -0,0 +1,17 @@
+"""
+Descending Order
+7kyu
+by TastyOs
+
+Your task is to make a function that can take any non-negative integer as a argument 
+and return it with its digits in descending order. 
+Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 21445 Output: 54421
+Input: 145263 Output: 654321
+Input: 1254859723 Output: 9875543221
+"""
+
+def Descending_Order(num):
+    return int("".join(map(str, sorted([i for i in str(num)], reverse=True))))
